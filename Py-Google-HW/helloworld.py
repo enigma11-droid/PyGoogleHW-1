9bb242d04dbfd15c0115c8e60c68f68a6f80f188
enigma11-droid <55906597+enigma11-droid@users.noreply.github.com>
2019-09-28 14:04:07 +0530

    Update helloworld.py

--- Py-Google-HW/helloworld.py
+++ Py-Google-HW/helloworld.py
@@ -1,2 +1,2 @@
 if __name__ == '__main__':
-    print('Hello World - welcome to NIIT University')
+    print('Hello World - welcome to NIIT University.The campus is lush green and the mountains have also become green by the efforts of our faculties')
